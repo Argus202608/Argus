d6fef962cdd77b54eda1fd8ea2370dbde4120014
MMArgus-Team <MMArgus-Team@users.noreply.github.com>
2026-08-21 16:39:59 +0800

    chore(release): bump to 0.1.1

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.1.0"
-__release_date__ = "2026.8.14"
+__version__ = "0.1.1"
+__release_date__ = "2026.8.21"
 
 
 def _ensure_utf8():
